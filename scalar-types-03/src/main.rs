5a9327fa51a290d98f2b63e38fac030081554497
sudo-von <sudo.von.contact@gmail.com>
2023-11-19 14:53:01 -0600

    feat: :sparkles: add content

---
+++ scalar-types-03/src/main.rs
@@ -0,0 +1,31 @@
+fn main() {
+    let x: i8 = -10;
+    println!("{}", x);
+
+    let y: u8 = 10;
+    println!("{}", y);
+
+    let decimal = 02_55;
+    println!("{}", decimal);
+
+    let hex = 0xff;
+    println!("{}", hex);
+
+    let octal = 0o377;
+    println!("{}", octal);
+
+    let binary = 0b1111_1111;
+    println!("{}", binary);
+
+    let byte = b'@';
+    println!("{}", byte);
+
+    let t = true;
+    println!("{}", t);
+
+    let f = false;
+    println!("{}", f);
+
+    let c = '@';
+    println!("{}", c);
+}
